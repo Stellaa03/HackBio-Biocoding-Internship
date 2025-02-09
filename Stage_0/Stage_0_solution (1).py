9b5d61eeee104ab7e40b53e20311dba47b15d773
Stellaa03 <ifeoluwastella02@gmail.com>
2025-02-09 09:19:12 +0100

    Add files via upload

---
+++ Stage_0/Stage_0_solution (1).py
@@ -0,0 +1,108 @@
+# Organizing the information using a list of dictionaries
+interns_details = [
+    {
+        "Name": "Muniru Elijah Taiwo",
+        "Slack Username": "MuniruElijah",
+        "Email": "muniruelijah@gmail.com",
+        "Hobby": "Reading",
+        "Country": "Nigeria",
+        "Discipline": "Anatomy",
+        "Preferred Programming Language": "R"
+    },
+    {
+        "Name": "Elias Korchi Meziani",
+        "Slack Username": "Elias",
+        "Email": "elias.k.meziani@gmail.com",
+        "Hobby": "Videogames, History, Life Sciences, Gym, Sleeping",
+        "Country": "Spain",
+        "Discipline": "Pharmacy",
+        "Preferred Programming Language": "Python"
+    },
+    {
+        "Name": "Toluwalase Taiwo",
+        "Slack Username": "Lase",
+        "Email": "princesstoluwalase@gmail.com",
+        "Hobby": "Watching movies, sleeping",
+        "Country": "Nigeria",
+        "Discipline": "Microbiology",
+        "Preferred Programming Language": "Python"
+    },
+    {
+        "Name": "Neo Mokgadi Mametja",
+        "Slack Username": "NeoMametja",
+        "Email": "nmametja@gmail.com",
+        "Hobby": "Outdoor activities, Gym and Sleeping",
+        "Country": "South Africa",
+        "Discipline": "Plant Biotechnology",
+        "Preferred Programming Language": "Python"
+    },
+    {
+        "Name": "Samuel Ogunleye",
+        "Slack Username": "Sproff",
+        "Email": "hellodevsproff@gmail.com",
+        "Hobby": "Music",
+        "Country": "Nigeria",
+        "Discipline": "Biochemistry",
+        "Preferred Programming Language": "Python"
+    },
+    {
+        "Name": "Stella Adediwura",
+        "Slack Username": "Stella",
+        "Email": "ifeoluwastella02@gmail.com",
+        "Hobby": "Sleeping, reading, watching movies and surfing the Internet",
+        "Country": "Nigeria",
+        "Discipline": "Microbiology",
+        "Preferred Programming Language": "Python"
+    }
+]
+
+# Simplified print command using multi-line strings
+print("""--------------------------------------------------
+Name: Muniru Elijah Taiwo
+Slack Username: MuniruElijah
+Email: muniruelijah@gmail.com
+Hobby: Reading
+Country: Nigeria
+Discipline: Anatomy
+Preferred Programming Language: R
+--------------------------------------------------
+Name: Elias Korchi Meziani
+Slack Username: Elias
+Email: elias.k.meziani@gmail.com
+Hobby: Videogames, History, Life Sciences, Gym, Sleeping
+Country: Spain
+Discipline: Pharmacy
+Preferred Programming Language: Python
+--------------------------------------------------
+Name: Toluwalase Taiwo
+Slack Username: Lase
+Email: princesstoluwalase@gmail.com
+Hobby: Watching movies, sleeping
+Country: Nigeria
+Discipline: Microbiology
+Preferred Programming Language: Python
+--------------------------------------------------
+Name: Neo Mokgadi Mametja
+Slack Username: NeoMametja
+Email: nmametja@gmail.com
+Hobby: Outdoor activities, Gym and Sleeping
+Country: South Africa
+Discipline: Plant Biotechnology
+Preferred Programming Language: Python
+--------------------------------------------------
+Name: Samuel Ogunleye
+Slack Username: Sproff
+Email: hellodevsproff@gmail.com
+Hobby: Music
+Country: Nigeria
+Discipline: Biochemistry
+Preferred Programming Language: Python
+--------------------------------------------------
+Name: Stella Adediwura
+Slack Username: Stella
+Email: ifeoluwastella02@gmail.com
+Hobby: Sleeping, reading, watching movies and surfing the Internet
+Country: Nigeria
+Discipline: Microbiology
+Preferred Programming Language: Python
+--------------------------------------------------""")
